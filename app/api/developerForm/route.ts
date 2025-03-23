1c647e432ae352e8172b294e391ee7b4326df5ce
leonel <leonelyimga@gmail.com>
2025-03-23 13:55:44 +0100

    update ~ image gallery autorisation

--- app/api/developerForm/route.ts
+++ app/api/developerForm/route.ts
@@ -1,34 +1,13 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/db";
+import {prisma} from "@/lib/db"
+import { getDevelopersFom } from "@/app/controllers/developerform.controller";
 
 export async function GET() {
   try {
-    const developers = await prisma.formDeveloper.findMany();
-    return NextResponse.json(
-      { success: true, developers },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          'Pragma': 'no-cache',
-          'Expires': '0',
-          'Surrogate-Control': 'no-store'
-        }
-      }
-    );
+    const developers = await getDevelopersFom();
+    return NextResponse.json({ success: true, developers });
   } catch (error) {
     console.error('Error fetching developers:', error);
-    return NextResponse.json(
-      { success: false, error: (error as Error).message },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          'Pragma': 'no-cache',
-          'Expires': '0',
-          'Surrogate-Control': 'no-store'
-        }
-      }
-    );
+    return NextResponse.json({ success: false, error: (error as Error).message });
   }
 }
---
+++ app/controllers/developerform.controller.ts
@@ -0,0 +1,18 @@
+
+import { prisma } from "@/lib/db";
+export const getDevelopersFom = async () => {
+  try {
+    const formDevelopers = await prisma.formDeveloper.findMany()
+
+    if (!formDevelopers) {
+      throw new Error('formDevelopers not exist');
+    }
+
+    return formDevelopers;
+
+  } catch (error) {
+    throw new Error('Failed to retrieve user formDevelopers');
+  } finally {
+    await prisma.$disconnect();
+  }
+};
