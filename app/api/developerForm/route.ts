3a15d5d0dca2608b357f4d7f7d5ada30b09c30e1
leonel <leonelyimga@gmail.com>
2025-03-23 13:35:27 +0100

    update ~ image gallery autorisation

--- app/api/developerForm/route.ts
+++ app/api/developerForm/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
-import {prisma} from "@/lib/db"
+import { prisma } from "@/lib/db";
 
 export async function GET() {
   try {
     const developers = await prisma.formDeveloper.findMany();
-    return NextResponse.json({ success: true, developers });
+    return NextResponse.json({ success: true, developers }, { headers: { 'Content-Type': 'application/json' } });
   } catch (error) {
     console.error('Error fetching developers:', error);
-    return NextResponse.json({ success: false, error: (error as Error).message });
+    return NextResponse.json({ success: false, error: (error as Error).message }, { headers: { 'Content-Type': 'application/json' } });
   }
 }
